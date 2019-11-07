f6d3851df8b47ea5642d8b22f15138adbb243f17
Misra Turp <misraturp@gmail.com>
2019-11-07 23:31:04 +0100

    Updates

---
+++ precache-manifest.b2f7084482680a14789ccbb4ec163ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2b5ca45fc0f3ec9a76688b06961af7",
+    "url": "/shared-shopping-list-app/index.html"
+  },
+  {
+    "revision": "db23a0c5b2f1fb085662",
+    "url": "/shared-shopping-list-app/static/css/2.05f6e4a7.chunk.css"
+  },
+  {
+    "revision": "87974abf1da07a05572a",
+    "url": "/shared-shopping-list-app/static/css/main.496795fc.chunk.css"
+  },
+  {
+    "revision": "db23a0c5b2f1fb085662",
+    "url": "/shared-shopping-list-app/static/js/2.fe1d9c73.chunk.js"
+  },
+  {
+    "revision": "87974abf1da07a05572a",
+    "url": "/shared-shopping-list-app/static/js/main.503e2109.chunk.js"
+  },
+  {
+    "revision": "7f6c067b131edca81b7c",
+    "url": "/shared-shopping-list-app/static/js/runtime-main.c866893a.js"
+  }
+]);
