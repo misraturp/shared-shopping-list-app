141a6cb0b8b7daadae90f033bde1636c968ee826
Misra Turp <misraturp@gmail.com>
2020-01-16 13:12:15 +0100

    fix api url

--- src/variables.js
+++ src/variables.js
@@ -1,2 +1,2 @@
-export const APP_URL = 'https://shared-shopping-list-app.herokuapp.com';
+export const APP_URL = 'https://shared-shopping-list-api.herokuapp.com';
 // export const APP_URL = 'http://localhost:3030/shopping_lists'
